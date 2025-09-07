f4609be282e32b5a8d063aac4bfd1851b71eab99
Simon Walz <contact@simonwalz.de>
2025-09-07 04:40:22 +0200

    refact: create LXC image

--- create-lxc-image.sh
+++ create-lxc-image.sh
@@ -10,11 +10,39 @@ raise() {
 	exit 1
 }
 
+dockerlogin() {
+	## docker login:
+	UPDATE_REGISTRY="$(jq -r '.update_registry' "${CONFIG_PATH}")"
+	# just the server name:
+	UPDATE_REGISTRY_SERVER="${UPDATE_REGISTRY//\/*}"
+
+	UPDATE_REGISTRY_USER="$(jq -r '.update_registry_user // .update_server_user // ""' "${CONFIG_PATH}")"
+	UPDATE_REGISTRY_PASSWORD="$(jq -r '.update_registry_password // .update_server_password // ""' "${CONFIG_PATH}")"
+
+
+	# Do docker login only if not already in .docker/config.json file
+	if ! grep -q "${UPDATE_REGISTRY_SERVER}" "${HOME}/.docker/config.json" 2>/dev/null
+	then
+		echo "${UPDATE_REGISTRY_PASSWORD}" | docker login \
+			"${UPDATE_REGISTRY_SERVER}" \
+			--username "${UPDATE_REGISTRY_USER}" --password-stdin
+		if test "$?" != "0"
+		then
+			echo "Error: Docker login failed" >&2
+			exit 1
+		fi
+	else
+		echo "Credentials for ${UPDATE_REGISTRY_SERVER} already existing. Skipped login."
+	fi
+}
+
 export CONFIG_PATH="$1"
 if [[ ! -f "${CONFIG_PATH}" ]]; then
 	echo "Config file not found."
 	exit 1
 fi
+
+dockerlogin
 
 UPDATE_REGISTRY="$(jq -r '.update_registry' "${CONFIG_PATH}")"
 OS_IMAGE="$(jq -r '.os_image_lxc' "${CONFIG_PATH}")"
@@ -35,9 +63,12 @@ docker run -it -d \
 	--pull=never \
 	--restart=always \
 	--name "${CONTAINER_NAME}" \
-	"${IMAGE}" || raise "Failed to run container"
-docker exec "${CONTAINER_NAME}" /usr/local/cuos/first-run.sh "${PARTITION}" "${IMAGE_VERSION}" "${OS_DIGEST}" \
+	"${IMAGE_VERSION}" || raise "Failed to run container"
+docker exec "${CONTAINER_NAME}" /usr/local/cuos/first-run.sh "${PARTITION}" "${IMAGE_VERSION}" "${NEW_DIGEST}" \
 	|| raise "Failed to run first-run script in container"
+
+docker cp "${CONFIG_PATH}" "${CONTAINER_NAME}:/data/system_A.json" \
+	|| raise "Failed to copy system.json"
 
 if ! docker export "${CONTAINER_NAME}" | gzip >image.tar.gz; then
 	raise "Failed to export the container"
