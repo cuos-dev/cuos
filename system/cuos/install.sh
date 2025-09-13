cd33741e3639fb9d200ce2b8dd778dc16eb62ec3
Simon Walz <contact@simonwalz.de>
2025-09-13 12:18:58 +0200

    fix: cuos socket, after refactoring

--- system/cuos/install.sh
+++ system/cuos/install.sh
@@ -2,10 +2,10 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln -sf "${SCRIPT_DIR}"/cuos-init.service /etc/systemd/system/ || exit 1
-ln -sf "${SCRIPT_DIR}"/cuos-api.service /etc/systemd/system/ || exit 1
-ln -sf "${SCRIPT_DIR}"/cuos-api.socket /etc/systemd/system/ || exit 1
-ln -sf "${SCRIPT_DIR}"/cuos-app.service /etc/systemd/system/ || exit 1
+ln -sf "${SCRIPT_DIR}/cuos-init.service" /etc/systemd/system/ || exit 1
+ln -sf "${SCRIPT_DIR}/cuos-api@.service" /etc/systemd/system/ || exit 1
+ln -sf "${SCRIPT_DIR}/cuos-api.socket" /etc/systemd/system/ || exit 1
+ln -sf "${SCRIPT_DIR}/cuos-app.service" /etc/systemd/system/ || exit 1
 
 ln -sf "${SCRIPT_DIR}"/api.sh /usr/bin/cuos || exit 1
 
