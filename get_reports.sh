96ff2e169f88074f65c9cfe037561361b97a1d2f
Simon Walz <contact@simonwalz.de>
2025-09-24 20:43:11 +0200

    refact: error handling

---
+++ get_reports.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+get_reports_filter() {
+  grep report_ -- "$@" | sed -e 's/^[	 ]*report_//g' | sed -e 's#$#'" \"$1\""'#'
+  #| grep -v 'err "$@"'
+}
+export -f get_reports_filter
+
+find system updater \
+  -name \*.sh \
+  -not -name utils.sh \
+  -exec bash -c 'get_reports_filter "$@"' -- '{}' ';'
+
+#find system -name \*.sh -not -name utils.sh -exec grep report_ '{}' ';' | sed -e 's/^[	 ]*report_//g'
+
