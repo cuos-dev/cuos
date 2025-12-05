f2cd092dee04a0db6d070fbec4e36c043bb9b94b
Simon Walz <contact@simonwalz.de>
2025-12-05 22:24:01 +0100

    refact: startup complete message

--- system/cuos/api.sh
+++ system/cuos/api.sh
@@ -208,7 +208,7 @@ EOF
   "report_app_ready")
     if [[ "$(state '.starting')" == "true" ]]; then
       message="$(echo "${INPUT}" | jq -r '.message // ""')"
-      report_info "cuos:startup:done" "Startup complete${message+" – ${message}"}"
+      report_info "cuos:startup:done" "Startup complete${message+" - ${message}"}"
       state jq '.starting = false'
     fi
   ;;
