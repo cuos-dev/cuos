1e0b5e8b0cf7705c06de2c67f0ecb6a1d0ddcee3
Simon Walz <contact@simonwalz.de>
2026-03-12 01:00:30 +0100

    refact: imported correct keyring

--- system/raspberrypi-archive-update.sh
+++ system/raspberrypi-archive-update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl -fsSL https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o raspberrypi-archive.gpg
+rm raspberrypi-archive.gpg
+#invalid:
+#curl -fsSL https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o raspberrypi-archive.gpg
+
+curl -fsSL --output raspberrypi-archive.gpg "https://github.com/raspberrypi/rpi-image-gen/raw/refs/heads/master/keydir/raspberrypi-archive-keyring.gpg"
